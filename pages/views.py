9ab4a5406057b0bdcce0e31812e682228b5e02be
Alex S <faeyntt@gmail.com>
2024-10-17 11:57:06 -0600

    testing

--- pages/views.py
+++ pages/views.py
@@ -4,13 +4,17 @@ from accounts.models import CustomUser
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+    model = CustomUser
     
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         users = CustomUser.objects.all()
         
-        # print(users)
+        print(users)
         print(f"Users in context: {list(users)}")
         
         context['users'] = users
         return context
+
+    
